c789d6a534bbb95a695cba08f7e66c385e33f3db
Jieun <jeeneep@naver.com>
2025-10-20 22:27:52 +0900

    feat(app): add application entry point and main flow control

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import calculator.domain.StringCalculator;
+import calculator.parser.InputParser;
+import calculator.view.ConsoleView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        ConsoleView view = new ConsoleView();
+        InputParser parser = new InputParser();
+        StringCalculator calculator = new StringCalculator(parser);
+
+        String input = view.getInput();
+
+        int result = calculator.add(input);
+
+        view.printResult(result);
     }
 }
